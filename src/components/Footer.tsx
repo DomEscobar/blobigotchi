3da9f8c4d9acc9450a6c0e353c3b84856c9a9baf
domEscobar <neridonk@googlemail.com>
2025-03-19 14:27:35 +0100

    asd

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,11 +8,26 @@ interface FooterProps {
 
 const Footer: React.FC<FooterProps> = ({ className = '' }) => {
   const isMobile = useIsMobile();
-  
+
   return (
     <div className={`mt-3 md:mt-4 text-center ${className}`}>
       <p className={`${isMobile ? 'text-[9px]' : 'text-xs'} text-gray-400`}>
-        Take good care of your virtual pet! Interact regularly to help it evolve.
+        <a target="_blank" href="https://jam.pieter.com"
+          style={{
+            fontFamily: 'system-ui, sans-serif',
+            position: 'fixed',
+            bottom: '-1px',
+            right: '-1px',
+            padding: '7px',
+            fontSize: '14px',
+            fontWeight: 'bold',
+            background: '#fff',
+            color: '#000',
+            textDecoration: 'none',
+            zIndex: '10',
+            borderTopLeftRadius: '12px',
+            border: '1px solid #fff',
+          }}>🕹️ Vibe Jam 2025</a>
       </p>
     </div>
   );
